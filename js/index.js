a703f2acb43f05a01b436a57c63c6446857e2bd7
master <2376655185@qq.com>
2023-06-12 12:56:48 +0800

    修改了请求地址为http请求

--- js/index.js
+++ js/index.js
@@ -55,7 +55,7 @@ function init(result, page) {
 function getCityInfo(page) {
     // 调用封装的axios函数 发送请求
     axios({
-        url: './cityData.json'
+        url: 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json'
     }).then(result => {
         // 取前100条数据
         result = result.slice(0, 100);
